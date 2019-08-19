35d1c7604c82babfffe705674ca71945b69dd47a
EinGyinMoe <40414316@napier.ac.uk>
2019-08-19 14:41:45 +0630

    Added Department class (lab6)

---
+++ src/main/java/com/napier/sem/Department.java
@@ -0,0 +1,18 @@
+package com.napier.sem;
+
+public class Department {
+    /**
+     * Department number
+     */
+    public int dept_no;
+
+    /**
+     * Department name
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public Employee manager;
+}
